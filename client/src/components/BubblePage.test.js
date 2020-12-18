3b5cc5f4018ac7967264d83ffa5d5fa55cce952d
reesharper <reestimharper@gmail.com>
2020-12-18 13:27:27 -0500

    tests now passing

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -2,6 +2,33 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 
-test("Fetches data and renders the bubbles", () => {
-  // Finish this test
+import { BubblePage as mockGetBubbles } from './BubblePage'
+
+const data = {
+  data:[
+    {
+      color: "aqua",
+      code: {
+        hex: "#00ffff"
+      },
+      id: 3
+    },
+    {
+      color: "aquamarine",
+      code: {
+        hex: "#7fffd4"
+      },
+      id: 4
+    }
+  ]
+}
+
+test("Fetches data and renders the bubbles", async () => {
+
+  mockGetBubbles.mockResolvedValueOnce(data);
+  render(<BubblePage/>);
+
+  const aqua = screen.getByText(/aqua/i);
+  expect(aqua).toExist();
+
 });

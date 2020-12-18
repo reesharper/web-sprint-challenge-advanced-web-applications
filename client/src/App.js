eff80aeaac1b67bc4b00f5bd5e5d4a771a114d78
reesharper <reestimharper@gmail.com>
2020-12-18 12:31:01 -0500

    set up protected route and login page routing to the bubbles page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import BubblePage from "./components/BubblePage";
 
 import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 import "./styles.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
+      <Switch>
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <ProtectedRoute exact path='/protected' component={BubblePage} />
+      </Switch>
       </div>
     </Router>
   );
